fd9048a2da4fee983e133469f6a6672cf3b1908a
Robin Schroer <git@sulami.xyz>
2022-12-10 16:25:21 +0100

    Clean up day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,18 @@
 pub fn solve() {
     let input = include_str!("../inputs/01.txt");
+    println!("day 1-1: {}", part1(input));
+    println!("day 1-2: {}", part2(input));
+}
+
+fn part1(input: &str) -> u32 {
+    *elf_calories(input).last().unwrap()
+}
+
+fn part2(input: &str) -> u32 {
+    elf_calories(input).iter().rev().take(3).sum()
+}
+
+fn elf_calories(input: &str) -> Vec<u32> {
     let count_calories = |elf: &str| -> u32 {
         elf.split_whitespace()
             .map(|n| n.parse::<u32>().expect("failed to parse calories"))
@@ -7,9 +20,36 @@ pub fn solve() {
     };
     let mut elves: Vec<u32> = input.split("\n\n").map(count_calories).collect();
     elves.sort();
-    println!(
-        "day 1-1: {}\nday 1-2: {}",
-        elves.last().unwrap(),
-        elves.iter().rev().take(3).sum::<u32>()
-    );
+    elves
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(INPUT), 24000);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(INPUT), 45000);
+    }
 }
